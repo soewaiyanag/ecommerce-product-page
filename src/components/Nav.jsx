036792df0af58f2cb59f8e0ce4789a734b3b6974
soewaiyanag <soewaiyanag@gmail.com>
2022-01-21 15:06:00 +0630

    indicator based on path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,27 @@
+import { useRef, useEffect } from "react";
 import "./Nav.scss";
 import menu from "../images/icon-menu.svg";
 import cart from "../images/icon-cart.svg";
 import avatar from "../images/image-avatar.png";
 import logo from "../images/logo.svg";
 import { Link, useLocation } from "react-router-dom";
+import capitalize from "lodash.capitalize";
+
+const navLinks = ["collections", "men", "women", "about", "contact"];
 
 const Nav = () => {
   const path = useLocation().pathname;
+  const navLinksRef = useRef(null);
+
+  useEffect(() => {
+    navLinks.forEach((navLink, index) => {
+      if (path === `/${navLink}`) {
+        navLinksRef.current.childNodes[index].classList.add("current");
+      } else {
+        navLinksRef.current.childNodes[index].classList.remove("current");
+      }
+    });
+  });
 
   return (
     <nav className="Nav">
@@ -14,26 +29,12 @@ const Nav = () => {
       <Link to="/">
         <img className="Nav__logo" src={logo} alt="logo" />
       </Link>
-      <div className="Nav__links">
-        <Link className="Nav__link current" to="collections">
-          Collections
-        </Link>
-
-        <Link className="Nav__link" to="men">
-          Men
-        </Link>
-
-        <Link className="Nav__link" to="women">
-          Women
-        </Link>
-
-        <Link className="Nav__link" to="about">
-          About
-        </Link>
-
-        <Link className="Nav__link" to="Contact">
-          Collections
-        </Link>
+      <div className="Nav__links" ref={navLinksRef}>
+        {navLinks.map((navLink) => (
+          <Link className="Nav__link" to={"/" + navLink} key={navLink}>
+            {capitalize(navLink)}
+          </Link>
+        ))}
       </div>
       <Link to="/cart" className="Nav__cart">
         <img src={cart} alt="cart" />
