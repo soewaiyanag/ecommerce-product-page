b64435a5d7d4d342bc76c2f8e2fbc2c945be90f8
soewaiyanag <soewaiyanag@gmail.com>
2022-01-23 14:05:53 +0630

    Main mobile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Main.scss";
 import cart from "../images/icon-cart.svg";
+import plus from "../images/icon-plus.svg";
+import minus from "../images/icon-minus.svg";
 
 const Main = () => {
   const [amount, setAmount] = useState(0);
@@ -17,21 +19,44 @@ const Main = () => {
         <h1 className="Main__price--sell">$125.00</h1>
         <span className="Main__price--actual">$250.00</span>
       </div>
-      <div className="Main__amount">
-        <input
-          className="Main__amount--input"
-          value={amount}
-          placeholder={0}
-          onChange={(e) => {
-            let value = Number(e.target.value);
-            value = value === 0 ? "" : value;
-            setAmount(value);
-          }}
-          type="number"
-          name="amount"
-          id="amount"
-        />
-        <button className="Main__amount--btn">
+      <div className="Main--foot">
+        <div className="Main__amount">
+          <img
+            onClick={() => {
+              let value = Number(amount);
+              if (value <= 0) return;
+              setAmount(value - 1);
+            }}
+            src={minus}
+            alt="minus"
+            className="Main__amount--minus"
+          />
+          <input
+            className="Main__amount--input"
+            placeholder={0}
+            min={0}
+            max={999}
+            maxLength={4}
+            value={amount}
+            onChange={(e) => {
+              let value = e.target.value;
+              value = value === "00" ? "0" : value;
+              setAmount(value);
+            }}
+            type="number"
+            name="amount"
+            id="amount"
+          />
+          <img
+            onClick={() => {
+              setAmount(Number(amount) + 1);
+            }}
+            src={plus}
+            alt="plus"
+            className="Main__amount--plus"
+          />
+        </div>
+        <button>
           <img src={cart} alt="cart icon" />
           Add to cart
         </button>
